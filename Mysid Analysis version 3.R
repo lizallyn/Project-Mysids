494a40792e717c2c62e64576de0407564cfb505b
Liz Allyn <liz.allyn@allyn.org>
2023-08-11 15:56:07 -0700

    Fixed effects added

--- Mysid Analysis version 3.R
+++ Mysid Analysis version 3.R
@@ -7,7 +7,7 @@
 # from file
 # data <- data <- read.csv("Er prey analysis for R fixed whale presence.csv")
 # from GitHub repo
-data <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Er%20prey%20analysis%20for%20R%20fixed%20whale%20presence.csv?token=GHSAT0AAAAAACFRBFAJSQ6NKD2A3Y6VTLAYZGAJMRA")
+data <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Er%20prey%20analysis%20for%20R%20fixed%20whale%20presence.csv?token=GHSAT0AAAAAACGGT3YBL6TD7PYVKVQJFPTEZGWVSHQ")
 
 ### Data Manipulation/Cleaning/Visualization
 
@@ -56,20 +56,20 @@ halfnorm(hval, labs = nonzero$Sample, nlab = length(uvals))
 
 # Construct hurdle model with ZT neg binom and assess Site as random effect.
 # Assess with LRT
-install.packages("RLRsim")
-library(TMB)
-library(glmmTMB)
-library(RLRsim)
+# install.packages("RLRsim")
+library(TMB) # glmmTMB needs this
+library(glmmTMB) # glmmTMB for model construction
+library(RLRsim) # lrtest
 
 # model with the random effect of Site
-model.rand <- glmmTMB(data = data, whalecount ~ MysidCount + StudyMon + (1|Site), 
+model.rand <- glmmTMB(data = data, whalecount ~ scale(MysidCount) + StudyMon + (1|Site), 
                   family = truncated_nbinom1, ziformula = ~.)
 summary(model.rand)
 model.matrix(model.rand)
 # returns design matrix
 
 # model without the random effect of Site
-model.norand <- glmmTMB(data = data, whalecount ~ MysidCount + StudyMon, 
+model.norand <- glmmTMB(data = data, whalecount ~ scale(MysidCount) + StudyMon, 
                   family = truncated_nbinom1, ziformula = ~.)
 summary(model.norand)
 model.matrix(model.norand)
@@ -78,3 +78,15 @@ lrtest(model.rand, model.norand) # doesn't work
 # Site makes sense according to structure of data and finding a package to 
 # test random effect structure is turning into a pain in the butt so including
 
+### Fixed Effects
+
+data$Site <- as.factor(data$Site)
+glmm.mm <- glmmTMB(data = data, whalecount ~ scale(MysidCount) + StudyMon + (1|Site), family = truncated_nbinom1, ziformula = ~.)
+summary(glmm.mm)
+glmm.m <- glmmTMB(data = data, whalecount ~ scale(MysidCount) + (1|Site), family = truncated_nbinom1, ziformula = ~.)
+summary(glmm.m)
+glmm.0 <- glmmTMB(data = data, whalecount ~ 1 + (1|Site), family = truncated_nbinom1, ziformula = ~.)
+
+data$whalecount
+
+plot(data$whalecount, predict(glmm.mm))
