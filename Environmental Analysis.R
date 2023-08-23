c6344a39c0ea5c16c552a27d737f053e5d5c08e9
Liz Allyn <liz.allyn@allyn.org>
2023-08-23 10:38:34 -0700

    read in data

--- Environmental Analysis.R
+++ Environmental Analysis.R
@@ -2,4 +2,18 @@
 
 # Read in and clean chlorophyll data
 
-chlo19.6 <- read.csv()
+chlo19.5 <- read.csv("MY1DMM_CHLORA_2019-05-01_rgb_3600x1800.SS.csv")
+chlo19.6 <- read.csv("MY1DMM_CHLORA_2019-06-01_rgb_3600x1800.SS.csv")
+chlo19.7 <- read.csv("MY1DMM_CHLORA_2019-07-01_rgb_3600x1800.SS.csv")
+chlo19.8 <- read.csv("MY1DMM_CHLORA_2019-08-01_rgb_3600x1800.SS.csv")
+chlo19.9 <- read.csv("MY1DMM_CHLORA_2019-09-01_rgb_3600x1800.SS.csv")
+chlo19.10 <- read.csv("MY1DMM_CHLORA_2019-10-01_rgb_3600x1800.SS.csv")
+chlo19.11 <- read.csv("MY1DMM_CHLORA_2019-11-01_rgb_3600x1800.SS.csv")
+chlo20.5 <- read.csv("MY1DMM_CHLORA_2020-05-01_rgb_3600x1800.SS.csv")
+chlo20.6 <- read.csv("MY1DMM_CHLORA_2020-06-01_rgb_3600x1800.SS.csv")
+chlo20.7 <- read.csv("MY1DMM_CHLORA_2020-07-01_rgb_3600x1800.SS.csv")
+chlo20.8 <- read.csv("MY1DMM_CHLORA_2020-08-01_rgb_3600x1800.SS.csv")
+chlo20.9 <- read.csv("MY1DMM_CHLORA_2020-09-01_rgb_3600x1800.SS.csv")
+chlo20.10 <- read.csv("MY1DMM_CHLORA_2020-10-01_rgb_3600x1800.SS.csv")
+chlo20.11 <- read.csv("MY1DMM_CHLORA_2020-11-01_rgb_3600x1800.SS.csv")
+
