e14001cb3a32fd00355a295bb79d1b2bdd2940b4
Liz Allyn <liz.allyn@allyn.org>
2023-09-06 10:45:04 -0700

    added the rangefinder thing for long

--- Mysid Analysis version 3.R
+++ Mysid Analysis version 3.R
@@ -284,22 +284,102 @@ model.matrix(model.norand)
 full.whales <- read.csv("Whales in full survey area 2019 2020.csv")
 sites <- read.csv("Sample site coords for R.csv")
 
-full.whales %>%
-  group_by()
-between(x = sites$Dec.lat, left = sites$Min.lat, right = sites$Max.lat)
-
 coordmatch <- data.frame(matrix(nrow= nrow(full.whales)))
-coordmatch$site <- "Error"
 # add sighting coords and IDs
 coordmatch$sighting <- full.whales$Date_S
 coordmatch$lat <- full.whales$Start_Dec_Lat
+coordmatch$long <- full.whales$Start_Dec_Long
+# create columns for assigned site names
+coordmatch$site.lat <- "Error"
+coordmatch$site.long <- "Error"
 # add min lat columns
 coordmatch$AR.lat.min <- sites$Min.lat[which(sites$Location=="Anderson Rocks")]
 coordmatch$BB.lat.min <- sites$Min.lat[which(sites$Location=="Bullman Beach")]
 coordmatch$SB.lat.min <- sites$Min.lat[which(sites$Location=="South of Bodeltehs")]
+coordmatch$CB.lat.min <- sites$Min.lat[which(sites$Location=="Chito Beach")]
+coordmatch$DP.lat.min <- sites$Min.lat[which(sites$Location=="Duk Point")]
+coordmatch$NB.lat.min <- sites$Min.lat[which(sites$Location=="North of Bodeltehs")]
+coordmatch$FB.lat.min <- sites$Min.lat[which(sites$Location=="First Beach")]
+coordmatch$KL.lat.min <- sites$Min.lat[which(sites$Location=="Koitlah")]
+coordmatch$OI.lat.min <- sites$Min.lat[which(sites$Location=="Ozette Island")]
+coordmatch$PH.lat.min <- sites$Min.lat[which(sites$Location=="Portage Head")]
+coordmatch$SR.lat.min <- sites$Min.lat[which(sites$Location=="Sail River")]
+coordmatch$SS.lat.min <- sites$Min.lat[which(sites$Location=="Seal and Sail Rocks")]
+coordmatch$SK.lat.min <- sites$Min.lat[which(sites$Location=="Skagway Rocks")]
+coordmatch$SL.lat.min <- sites$Min.lat[which(sites$Location=="Slant Rock")]
 # add max lat columns
 coordmatch$AR.lat.max <- sites$Max.lat[which(sites$Location=="Anderson Rocks")]
 coordmatch$BB.lat.max <- sites$Max.lat[which(sites$Location=="Bullman Beach")]
 coordmatch$SB.lat.max <- sites$Max.lat[which(sites$Location=="South of Bodeltehs")]
+coordmatch$CB.lat.max <- sites$Max.lat[which(sites$Location=="Chito Beach")]
+coordmatch$DP.lat.max <- sites$Max.lat[which(sites$Location=="Duk Point")]
+coordmatch$NB.lat.max <- sites$Max.lat[which(sites$Location=="North of Bodeltehs")]
+coordmatch$FB.lat.max <- sites$Max.lat[which(sites$Location=="First Beach")]
+coordmatch$KL.lat.max <- sites$Max.lat[which(sites$Location=="Koitlah")]
+coordmatch$OI.lat.max <- sites$Max.lat[which(sites$Location=="Ozette Island")]
+coordmatch$PH.lat.max <- sites$Max.lat[which(sites$Location=="Portage Head")]
+coordmatch$SR.lat.max <- sites$Max.lat[which(sites$Location=="Sail River")]
+coordmatch$SS.lat.max <- sites$Max.lat[which(sites$Location=="Seal and Sail Rocks")]
+coordmatch$SK.lat.max <- sites$Max.lat[which(sites$Location=="Skagway Rocks")]
+coordmatch$SL.lat.max <- sites$Max.lat[which(sites$Location=="Slant Rock")]
+# assign site name if between range
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$AR.lat.min, coordmatch$AR.lat.max))] <- "Anderson Rocks"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$BB.lat.min, coordmatch$BB.lat.max))] <- "Bullman Beach"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$SB.lat.min, coordmatch$SB.lat.max))] <- "South of Bodeltehs"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$CB.lat.min, coordmatch$CB.lat.max))] <- "Chito Beach"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$DP.lat.min, coordmatch$DP.lat.max))] <- "Duk Point"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$NB.lat.min, coordmatch$NB.lat.max))] <- "North of Bodeltehs"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$FB.lat.min, coordmatch$FB.lat.max))] <- "First Beach"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$KL.lat.min, coordmatch$KL.lat.max))] <- "Koitlah"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$OI.lat.min, coordmatch$OI.lat.max))] <- "Ozette Island"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$PH.lat.min, coordmatch$PH.lat.max))] <- "Portage Head"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$SR.lat.min, coordmatch$SR.lat.max))] <- "Sail River"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$SS.lat.min, coordmatch$SS.lat.max))] <- "Seal and Sail Rocks"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$SK.lat.min, coordmatch$SK.lat.max))] <- "Skagway Rocks"
+coordmatch$site.lat[which(between(coordmatch$lat, coordmatch$SL.lat.min, coordmatch$SL.lat.max))] <- "Slant Rock"
 
-coordmatch$site[which(between(coordmatch$lat, coordmatch$AR.lat.min, coordmatch$AR.lat.max))] <- "Anderson"
+# add min long columns
+coordmatch$AR.long.min <- sites$Min.long[which(sites$Location=="Anderson Rocks")]
+coordmatch$BB.long.min <- sites$Min.long[which(sites$Location=="Bullman Beach")]
+coordmatch$SB.long.min <- sites$Min.long[which(sites$Location=="South of Bodeltehs")]
+coordmatch$CB.long.min <- sites$Min.long[which(sites$Location=="Chito Beach")]
+coordmatch$DP.long.min <- sites$Min.long[which(sites$Location=="Duk Point")]
+coordmatch$NB.long.min <- sites$Min.long[which(sites$Location=="North of Bodeltehs")]
+coordmatch$FB.long.min <- sites$Min.long[which(sites$Location=="First Beach")]
+coordmatch$KL.long.min <- sites$Min.long[which(sites$Location=="Koitlah")]
+coordmatch$OI.long.min <- sites$Min.long[which(sites$Location=="Ozette Island")]
+coordmatch$PH.long.min <- sites$Min.long[which(sites$Location=="Portage Head")]
+coordmatch$SR.long.min <- sites$Min.long[which(sites$Location=="Sail River")]
+coordmatch$SS.long.min <- sites$Min.long[which(sites$Location=="Seal and Sail Rocks")]
+coordmatch$SK.long.min <- sites$Min.long[which(sites$Location=="Skagway Rocks")]
+coordmatch$SL.long.min <- sites$Min.long[which(sites$Location=="Slant Rock")]
+# add max long columns
+coordmatch$AR.long.max <- sites$Max.long[which(sites$Location=="Anderson Rocks")]
+coordmatch$BB.long.max <- sites$Max.long[which(sites$Location=="Bullman Beach")]
+coordmatch$SB.long.max <- sites$Max.long[which(sites$Location=="South of Bodeltehs")]
+coordmatch$CB.long.max <- sites$Max.long[which(sites$Location=="Chito Beach")]
+coordmatch$DP.long.max <- sites$Max.long[which(sites$Location=="Duk Point")]
+coordmatch$NB.long.max <- sites$Max.long[which(sites$Location=="North of Bodeltehs")]
+coordmatch$FB.long.max <- sites$Max.long[which(sites$Location=="First Beach")]
+coordmatch$KL.long.max <- sites$Max.long[which(sites$Location=="Koitlah")]
+coordmatch$OI.long.max <- sites$Max.long[which(sites$Location=="Ozette Island")]
+coordmatch$PH.long.max <- sites$Max.long[which(sites$Location=="Portage Head")]
+coordmatch$SR.long.max <- sites$Max.long[which(sites$Location=="Sail River")]
+coordmatch$SS.long.max <- sites$Max.long[which(sites$Location=="Seal and Sail Rocks")]
+coordmatch$SK.long.max <- sites$Max.long[which(sites$Location=="Skagway Rocks")]
+coordmatch$SL.long.max <- sites$Max.long[which(sites$Location=="Slant Rock")]
+# assign site name if between range
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$AR.long.min, coordmatch$AR.long.max))] <- "Anderson Rocks"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$BB.long.min, coordmatch$BB.long.max))] <- "Bullman Beach"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$SB.long.min, coordmatch$SB.long.max))] <- "South of Bodeltehs"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$CB.long.min, coordmatch$CB.long.max))] <- "Chito Beach"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$DP.long.min, coordmatch$DP.long.max))] <- "Duk Point"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$NB.long.min, coordmatch$NB.long.max))] <- "North of Bodeltehs"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$FB.long.min, coordmatch$FB.long.max))] <- "First Beach"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$KL.long.min, coordmatch$KL.long.max))] <- "Koitlah"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$OI.long.min, coordmatch$OI.long.max))] <- "Ozette Island"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$PH.long.min, coordmatch$PH.long.max))] <- "Portage Head"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$SR.long.min, coordmatch$SR.long.max))] <- "Sail River"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$SS.long.min, coordmatch$SS.long.max))] <- "Seal and Sail Rocks"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$SK.long.min, coordmatch$SK.long.max))] <- "Skagway Rocks"
+coordmatch$site.long[which(between(coordmatch$long, coordmatch$SL.long.min, coordmatch$SL.long.max))] <- "Slant Rock"
