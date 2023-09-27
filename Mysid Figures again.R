9b6766cb7f1ae175cda19477a40590af443d55e8
Liz Allyn <liz.allyn@allyn.org>
2023-09-26 17:04:40 -0700

    migrated map with survey area to current figure script

--- Mysid Figures again.R
+++ Mysid Figures again.R
@@ -3,8 +3,82 @@
 
 ### Figures
 
+sites <- read.csv("Sample site coords for R.csv")
 tows <- read.csv("Tow data for R.csv")
 whale <- read.csv("Whales in full survey area 2019 2020.csv")
+
+## Sample sites map with survey area
+
+# packages needed: ggmap, ggrepel
+
+# bounds for sample map
+maxlong <- -124.9
+minlat <- 48.1
+minlong <- -124
+maxlat <- 48.45
+
+# bounds for inset map
+insleft <- -127
+insright <- -121
+instop <- 50
+insbott <- 45
+
+insetbox.lat <- c(minlat, minlat, maxlat, maxlat, minlat)
+insetbox.long <- c(minlong, maxlong, maxlong, minlong, minlong)
+insetbox.shape <- data.frame(cbind(insetbox.lat, insetbox.long))
+
+asurv1 <- 48.13
+asurv2 <- 48.13
+asurv3 <- 48.45
+asurv4 <- 48.38
+asurv5 <- 48.28
+
+osurv1 <- -124.71
+osurv2 <- -124.82
+osurv3 <- -124.82
+osurv4 <- -124.4
+osurv5 <- -124.4
+
+survlat <- c(asurv1, asurv2, asurv3, asurv4, asurv5)
+survlong <- c(osurv1, osurv2, osurv3, osurv4, osurv5)
+survpath <- data.frame(cbind(survlat, survlong))
+
+outline.lat <- c(insbott, insbott, instop, instop, insbott)
+outline.long <- c(insleft, insright, insright, insleft, insleft)
+outline <- data.frame(cbind(outline.lat, outline.long))
+
+inset <- get_stamenmap(bbox=c(insleft, insbott, insright, instop), 
+                       zoom=7, maptype="terrain")
+insetmap <- ggmap(inset) +
+  geom_path(data = insetbox.shape, aes(x = insetbox.long, y = insetbox.lat), size = 1) +
+  theme_void() +
+  geom_path(data = outline, aes(x = outline.long, y = outline.lat), size = 1.5)
+
+base_ter <- get_stamenmap(bbox = c(maxlong,minlat,minlong,maxlat), 
+                          zoom=11, maptype="terrain")
+map1 <- ggmap(base_ter) +
+  geom_point(data = sites, 
+             aes(x = Dec.long, y = Dec.lat), 
+             color = "white") +
+  geom_path(data = survpath, aes(x = survlong, y = survlat), size = 1, color = "gray") + 
+  labs(x = "Longitude", y = "Latitude") +
+  geom_text_repel(data = sites,
+                  alpha = 0.8,
+                  color = "black",
+                  aes(x = Dec.long, y = Dec.lat, label = Location),
+                  segment.linetype = 0,
+                  force_pull = 1,
+                  box.padding = 0.1,
+                  size = 3,
+                  fontface = 2)
+map_with_inset <- ggdraw() + 
+  draw_plot(map1) + 
+  draw_plot(insetmap, x = 0.65, y = 0.55, 
+            width = 0.4, height=0.4)
+map_with_inset
+
+# ggsave(plot = map_with_inset, filename = "Sample sites map final.pdf",
+#        width = 9, height = 5, device='pdf', dpi=700)
 
 ## Prey density map combined years and months
 
