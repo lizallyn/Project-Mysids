4dafe5e0abe441d20519af255192745d16aa5a2b
Liz Allyn <liz.allyn@allyn.org>
2023-10-03 11:15:18 -0700

    cleaned up sample site map with whale survey area outline

--- Mysid Figures again.R
+++ Mysid Figures again.R
@@ -3,18 +3,23 @@
 
 ### Figures
 
-sites <- read.csv("Sample site coords for R.csv")
-tows <- read.csv("Tow data for R.csv")
-whale <- read.csv("Whales in full survey area 2019 2020.csv")
+sites <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Sample%20site%20coords%20for%20R.csv")
+tows <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Tow%20data%20for%20R.csv")
+whale <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Whales%20in%20full%20survey%20area%202019%202020.csv")
 
-## Sample sites map with survey area
+library(ggmap)
+library(ggrepel)
+library(ggplot2)
+library(cowplot)
+
+## Sample sites map with whale survey area outline
 
 # packages needed: ggmap, ggrepel
 
 # bounds for sample map
 maxlong <- -124.9
 minlat <- 48.1
-minlong <- -124
+minlong <- -124.2
 maxlat <- 48.45
 
 # bounds for inset map
@@ -29,14 +34,14 @@ insetbox.shape <- data.frame(cbind(insetbox.lat, insetbox.long))
 
 asurv1 <- 48.13
 asurv2 <- 48.13
-asurv3 <- 48.43
-asurv4 <- 48.43
-asurv5 <- 48.36
-asurv6 <- 48.29
+asurv3 <- 48.44
+asurv4 <- 48.44
+asurv5 <- 48.35
+asurv6 <- 48.291
 
-osurv1 <- -124.71
-osurv2 <- -124.82
-osurv3 <- -124.82
+osurv1 <- -124.705
+osurv2 <- -124.85
+osurv3 <- -124.85
 osurv4 <- -124.6
 osurv5 <- -124.4
 osurv6 <- -124.4
@@ -50,19 +55,19 @@ outline.long <- c(insleft, insright, insright, insleft, insleft)
 outline <- data.frame(cbind(outline.lat, outline.long))
 
 inset <- get_stamenmap(bbox=c(insleft, insbott, insright, instop), 
-                       zoom=7, maptype="terrain")
+                       zoom=7, maptype="terrain-background")
+base_ter <- get_stamenmap(bbox = c(maxlong, minlat, minlong, maxlat), 
+                          zoom=11, maptype="terrain-background")
 insetmap <- ggmap(inset) +
-  geom_path(data = insetbox.shape, aes(x = insetbox.long, y = insetbox.lat), size = 1) +
+  geom_path(data = insetbox.shape, aes(x = insetbox.long, y = insetbox.lat), lwd = 0.75) +
   theme_void() +
-  geom_path(data = outline, aes(x = outline.long, y = outline.lat), size = 1.5)
+  geom_path(data = outline, aes(x = outline.long, y = outline.lat), lwd = 1.5)
 
-base_ter <- get_stamenmap(bbox = c(maxlong,minlat,minlong,maxlat), 
-                          zoom=11, maptype="terrain")
 map1 <- ggmap(base_ter) +
   geom_point(data = sites, 
              aes(x = Dec.long, y = Dec.lat), 
-             color = "white") +
-  geom_path(data = survpath, aes(x = survlong, y = survlat), size = 1, color = "gray") + 
+             color = "gray25") +
+  geom_path(data = survpath, aes(x = survlong, y = survlat), lwd = 1, color = "gray25") + 
   labs(x = "Longitude", y = "Latitude") +
   geom_text_repel(data = sites,
                   alpha = 0.8,
@@ -70,17 +75,17 @@ map1 <- ggmap(base_ter) +
                   aes(x = Dec.long, y = Dec.lat, label = Location),
                   segment.linetype = 0,
                   force_pull = 1,
-                  box.padding = 0.1,
+                  box.padding = 0.2,
                   size = 3,
                   fontface = 2)
 map_with_inset <- ggdraw() + 
   draw_plot(map1) + 
-  draw_plot(insetmap, x = 0.65, y = 0.55, 
-            width = 0.4, height=0.4)
+  draw_plot(insetmap, x = 0.72, y = 0.15, 
+            width = 0.3, height=0.3)
 map_with_inset
 
-# ggsave(plot = map_with_inset, filename = "Sample sites map final.pdf",
-#        width = 9, height = 5, device='pdf', dpi=700)
+ggsave(plot = map_with_inset, filename = "C:/Users/Elizabeth Allyn/Box/Makah Fisheries Management/Er prey/Liz Needs These Uploaded/Manuscript Docs/Review/Figures/Sample site map revision.pdf", 
+       width = 10, height = 9, device='pdf', dpi=700)
 
 ## Prey density map combined years and months
 
