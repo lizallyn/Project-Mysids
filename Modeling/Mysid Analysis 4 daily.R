b7249fd3c4b0e508dd54d1f5e1dc712f7ecd91df
Liz Allyn <liz.allyn@allyn.org>
2023-11-16 11:37:11 -0800

    updated data calls - one file is in recycle bin!!

--- Modeling/Mysid Analysis 4 daily.R
+++ Modeling/Mysid Analysis 4 daily.R
@@ -3,9 +3,9 @@
 
 # read in data
 
-full.whales <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Whales%20in%20full%20survey%20area%202019%202020.csv")
-whales <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/whales%20per%20day%20for%20R.csv")
-data.full <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Er%20prey%20analysis%20for%20R%20fixed%20whale%20presence.csv")
+full.whales <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Data%20Files/Whales%20in%20full%20survey%20area%202019%202020.csv")
+whales <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Recycle%20Bin/whales%20per%20day%20for%20R.csv")
+data.full <- read.csv("https://raw.githubusercontent.com/lizallyn/Project-Mysids/main/Data%20Files/Er%20prey%20analysis%20for%20R%20fixed%20whale%20presence.csv")
 # Pull out useful clean mysid data columns to simplify data frame
 data <- data.full[,c(1,2,4:6,7,11,17:25,30,31)]
 data$Site <- factor(data$Site, 
